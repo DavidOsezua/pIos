d78db100935efd80486e665b7c4a4975cf59bca2
DavidOsezua <102161226+DavidOsezua@users.noreply.github.com>
2025-04-07 18:54:16 +0100

    ...

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -15,7 +15,7 @@ const Homepage = () => {
 
         <section className={`section`}>
           <div className="sectionContainer space-y-20 px-6 lg:px-0">
-            <h3>
+            <h3 className={`font-bold text-accentone text-[1.5rem]`}>
               PLOS is a non-profit organization on a mission to drive open
               science forward with measurable, meaningful change in research
               publishing, policy and practice.
@@ -25,7 +25,9 @@ const Homepage = () => {
               className={` space-y-5 md:flex md:flex-row md:justify-between md:items-center`}
             >
               <div className={`space-y-5`}>
-                <h3>A catalyst for better</h3>
+                <h3 className={`font-bold text-accentone text-[2rem]`}>
+                  A catalyst for better
+                </h3>
                 <p className={`md:max-w-[540px]`}>
                   Built on a strong legacy of pioneering innovation, we see the
                   world through a lens of possibility, but we are not interested
@@ -51,7 +53,9 @@ const Homepage = () => {
 
         <section className={`section`}>
           <div className={`sectionContainer space-y-5 px-[1rem]`}>
-            <h3>Open science</h3>
+            <h3 className={`font-bold text-accentone text-[2rem]`}>
+              Open science
+            </h3>
 
             <p>
               PLOS is a mission-driven open science publisher covering all
@@ -70,9 +74,13 @@ const Homepage = () => {
             >
               {homePageOpenScience.map((data) => (
                 <div className={`space-y-3`}>
-                  <h2>{data.title}</h2>
+                  <h2
+                    className={`font-bold text-accentone text-[1.5rem]  border-b-[6px] pb-[0.6rem] border-b-[#F03F10]`}
+                  >
+                    {data.title}
+                  </h2>
 
-                  <h4>{data.subTitle}</h4>
+                  <h4 className={`font-bold  text-[1rem]`}>{data.subTitle}</h4>
 
                   <p className={`md:max-w-[330px]`}>{data.text}</p>
                 </div>
